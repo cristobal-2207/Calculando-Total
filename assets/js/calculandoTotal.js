2acb974ac3eeb535c0c6d61fd19ae9614840793c
Cristóbal Paredes <cristobalgutierreznavarro@gmail.com>
2022-10-11 17:25:27 -0300

    4to Commit: Se genera algoritmo para que se refleje el total de la compra y el color seleccionado.

--- assets/js/calculandoTotal.js
+++ assets/js/calculandoTotal.js
@@ -1,4 +1,13 @@
 const cantidadInput = document.querySelector(".cantidad")
 const colorInput = document.querySelector("#colorInput")
-const precio = document.querySelector(".precio")
+const precio = 30000
+const cantidadSpan = document.querySelector(".cantidad-span")
+const botonCalcular = document.querySelector("#btn-calcular")
+const totalSpan = document.querySelector(".total-calculado")
+const colorFinal = document.querySelector(".color-final")
 
+botonCalcular.addEventListener("click", () => {
+  cantidadSpan.textContent = cantidadInput.value;
+  totalSpan.textContent = `${cantidadInput.value * precio}`;
+  colorFinal.style.backgroundColor = colorInput.value;
+})
